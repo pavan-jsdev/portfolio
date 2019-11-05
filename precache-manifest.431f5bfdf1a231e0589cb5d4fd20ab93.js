ed824ba1b742ab56120e9f16c6ff4f945ae7645b
JDA\1025931 <PavanKumar.Divvela@jda.com>
2019-11-05 20:33:19 +0530

    Updates

---
+++ precache-manifest.431f5bfdf1a231e0589cb5d4fd20ab93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c28a764f9196dcfd134564a4ecfe97a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c4235310b8a002164a63",
+    "url": "/portfolio/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "41426a733a6b1d722828",
+    "url": "/portfolio/static/css/main.f8f4a6f2.chunk.css"
+  },
+  {
+    "revision": "c4235310b8a002164a63",
+    "url": "/portfolio/static/js/2.05e35abf.chunk.js"
+  },
+  {
+    "revision": "41426a733a6b1d722828",
+    "url": "/portfolio/static/js/main.0845e1ca.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
